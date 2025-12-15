19fab017817eb71579e1af09cf853c66dc9fbc0e
thearrasoverlord-netizen <thearrasoverlord@gmail.com>
2025-12-15 08:40:58 -0300

    Fix formatting and syntax in index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function getBalance(userId) {
 
 function setBalance(userId, amount) {
   balances.set(userId, amount);
-
+}
 // ⬇️ PEGA ESTO AQUÍ
 function rollMineral() {
   const total = minerals.reduce((sum, m) => sum + m.chance, 0);
